f3b46107c3eb9f345158279cfb2cbd69a0d7a39d
zahir-imt <zahir_wp@yahoo.ca>
2026-02-04 17:11:01 -0500

    initial commit

---
+++ src/main/java/com/github/action/SpringbootGithubAction1Application.java
@@ -0,0 +1,29 @@
+package com.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootGithubAction1Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubAction1Application.class, args);
+	}
+
+	echo "# springboot-gitaction-1" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/zahir-imt/springboot-gitaction-1.git
+	git push -u origin main
+
+}
---
+++ src/test/java/com/github/action/SpringbootGithubAction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubAction1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
